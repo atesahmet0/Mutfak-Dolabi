defb8037a25afb2f4890635679399d2ab2b8636d
atesahmet0 <ahmetrizaates@hotmail.com>
2021-06-15 21:01:47 +0300

    Implemented recyclerview, testing with random dataSet

---
+++ app/src/main/java/com/bilsem/mutfakdolabi/adapters/RecyclerViewAdapterOrder.kt
@@ -0,0 +1,37 @@
+package com.bilsem.mutfakdolabi.adapters
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bilsem.mutfakdolabi.R
+import com.bilsem.mutfakdolabi.objects.Order
+import kotlinx.android.synthetic.main.recyclerview_order_title.view.*
+
+class RecyclerViewAdapterOrder(val ordersList: List<Order>) :
+    RecyclerView.Adapter<RecyclerViewAdapterOrder.RecyclerViewAdapterOrderViewHolder>() {
+
+    inner class RecyclerViewAdapterOrderViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        fun bindView(order: Order) {
+            itemView.textViewRecyclerViewOrderTitleTitleOfGroup.text = order.title
+        }
+    }
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerViewAdapterOrderViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.recyclerview_order_title, parent, false)
+        return RecyclerViewAdapterOrderViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewAdapterOrderViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        return ordersList.size
+    }
+}
--- app/src/main/java/com/bilsem/mutfakdolabi/fragments/TalepListFragment.kt
+++ app/src/main/java/com/bilsem/mutfakdolabi/fragments/TalepListFragment.kt
@@ -7,20 +7,22 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.bilsem.mutfakdolabi.R
-import com.bilsem.mutfakdolabi.adapters.RecyclerViewAdapterGrup
+import com.bilsem.mutfakdolabi.adapters.RecyclerViewAdapterOrder
 import com.bilsem.mutfakdolabi.objects.Grup
+import com.bilsem.mutfakdolabi.objects.Order
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ListenerRegistration
 import kotlinx.android.synthetic.main.fragment_talep_list.view.*
 
 class TalepListFragment : Fragment() {
-    companion object{
+    companion object {
         const val TAG = "TALEPLISTFRAGMENT"
     }
 
     private var groupsOfCurrentUser = arrayListOf<Grup>()
     private val firebaseAuth = FirebaseAuth.getInstance()
-    val adapter = RecyclerViewAdapterGrup(groupsOfCurrentUser)
+    private val orderList = arrayListOf<Order>()
+    private val adapterRecyclerViewAdapterOrder = RecyclerViewAdapterOrder(orderList)
 
     private val detachList = arrayListOf<ListenerRegistration>()
 
@@ -29,14 +31,18 @@ class TalepListFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_talep_list,container,false)
+        return inflater.inflate(R.layout.fragment_talep_list, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
         view.recyclerViewOrderList.layoutManager = LinearLayoutManager(context)
-        view.recyclerViewOrderList.adapter = adapter
+        view.recyclerViewOrderList.adapter = adapterRecyclerViewAdapterOrder
+        for (i in 1..5) {
+            orderList.add(Order("Ev ${i}"))
+            adapterRecyclerViewAdapterOrder.notifyItemInserted(i)
+        }
 
 
     }
