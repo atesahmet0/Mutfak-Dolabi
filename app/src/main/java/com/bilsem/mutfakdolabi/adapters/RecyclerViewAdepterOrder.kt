7c2b901a71ecc79d63b67cd604aec35d0b795e4f
atesahmet0 <ahmetrizaates@hotmail.com>
2021-06-15 20:46:27 +0300

    Implemented base methods of the adapter

--- app/src/main/java/com/bilsem/mutfakdolabi/adapters/RecyclerViewAdepterOrder.kt
+++ app/src/main/java/com/bilsem/mutfakdolabi/adapters/RecyclerViewAdepterOrder.kt
@@ -1,3 +1,27 @@
 package com.bilsem.mutfakdolabi.adapters
 
-class RecyclerViewAdepterOrder
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class RecyclerViewAdepterOrder :
+    RecyclerView.Adapter<RecyclerViewAdepterOrder.RecyclerViewAdapterOrderViewHolder>() {
+
+    inner class RecyclerViewAdapterOrderViewHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerViewAdapterOrderViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: RecyclerViewAdapterOrderViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
